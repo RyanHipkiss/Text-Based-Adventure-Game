06210ac9ce6be0c26e1927df244fd67263e46670
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2017-06-09 07:57:26 +0100

    Initial Commit

---
+++ TheDarkRoom/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDarkRoom
+{
+    class Character
+    {
+        private string name;
+        private string className;
+        private int age;
+
+        public void Character(string name)
+        {
+            this.name = name;
+        }
+
+        public void setClass(string className)
+        {
+            this.className = className;
+        }
+
+        public void setAge(int age)
+        {
+            this.age = age;
+        }
+    }
+}
